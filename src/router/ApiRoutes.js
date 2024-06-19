f73996b02c289b5d0b22189a3d2e7519a1dcb509
tranminhtri <53221375+tranmintri@users.noreply.github.com>
2024-06-19 14:22:59 +0700

    Update ApiRoutes.js

--- src/router/ApiRoutes.js
+++ src/router/ApiRoutes.js
@@ -4,6 +4,6 @@ export const SERVICE_API = `${SERVER_HOST}/api/service/`;
 export const PRODUCT_API = `${SERVER_HOST}/api/product/`;
 export const BILL_API = `${SERVER_HOST}/api/bills/`;
 
-// export const SERVER_LOGIN_HOST = "https://nails-server-postgres.onrender.com";
-export const SERVER_LOGIN_HOST = "http://localhost:8081";
+export const SERVER_LOGIN_HOST = "https://nails-server-postgres.onrender.com";
+// export const SERVER_LOGIN_HOST = "http://localhost:8081";
 export const USER_API = `${SERVER_LOGIN_HOST}/api/user/`;
